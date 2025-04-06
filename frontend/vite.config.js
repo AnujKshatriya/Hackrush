2ec0b6aca06212efb2b2b01062a5d2c88dcf04d9
AnujKshatriya <anujkshatriyaiitian@gmail.com>
2025-04-06 05:36:01 +0530

    made pwa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,51 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        id: '/',
+        name: 'EventHub @ IITGN',
+        short_name: 'EventHub',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#2575fc',
+        description: 'Your one-stop solution for managing events, clubs, and communities at IITGN.',
+        icons: [
+          {
+            src: '/icons/iithub.jpg',
+            sizes: '360x360',
+            type: 'image/jpg',
+          },
+          {
+            src: '/icons/logo.webp',
+            sizes: '752x564',
+            type: 'image/webp',
+          },
+        ],
+        screenshots: [
+          {
+            src: '/screenshot/short-home.png',
+            sizes: '562x901',
+            type: 'image/png',
+            form_factor: 'narrow',
+          },
+          {
+            src: '/screenshot/wide-home.png',
+            sizes: '1916x905',
+            type: 'image/png',
+            form_factor: 'wide',
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true, // enables service worker in dev for testing
+      },
+    }),
+  ],
+});
